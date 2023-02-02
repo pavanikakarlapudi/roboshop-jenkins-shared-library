65064707cc6a82c16e7959a944fc3d1ec68dbd4c
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-02 11:29:41 +0530

    frontend jenkins

---
+++ vars/ci.groovy
@@ -0,0 +1,38 @@
+def call() {
+    pipeline {
+
+        agent {
+            label 'workstation'
+        }
+
+        stages {
+
+            stage('compile/build') {
+                steps{
+                    echo 'compile'
+                }
+            }
+
+            stage('unit tests') {
+                steps{
+                    echo 'unit tests'
+                }
+            }
+
+            stage('quality control') {
+                steps{
+                    echo 'quality control'
+                }
+            }
+            stage('upload code to centralised place'){
+                steps{
+                    echo 'upload'
+                }
+            }
+
+        }
+
+}
+
+
+
