d96c79026976680b23d98649522775fa4c668ecc
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-06 23:20:15 +0530

    frontend jenkins

---
+++ vars/ci-declarative.groovy
@@ -0,0 +1,59 @@
+def call() {
+    try {
+    pipeline {
+
+        agent {
+            label 'workstation'
+        }
+
+        stages {
+
+            stage('compile/build') {
+                steps {
+                   script {
+                       common.compile()
+                   }
+                }
+            }
+
+            stage('unit tests') {
+                steps {
+                    script {
+                        common.unittests()
+                    }
+                }
+            }
+
+            stage('quality control') {
+                environment {
+
+
+                    SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                    SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                }
+                steps {
+                    script {
+                        SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'',returnStdout: true).trim()
+                        wrap([$class: 'MaskPasswordsBuildWrapper',varPasswordPairs: [[password: "${SONAR_PASS}",var: 'SECRET']]]){
+                            sh "sonar-scanner -Dsonar.host.url=http://172.31.12.130:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                        }
+                    }
+
+
+                }
+            }
+            stage('upload code to centralised place') {
+                steps {
+                    echo 'upload'
+                }
+            }
+
+        }
+
+    }
+
+
+} catch(Exception e) {
+      common.email("failed")
+    }
+    }
--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,53 +1,40 @@
 def call() {
     try {
-    pipeline {
-
-        agent {
-            label 'workstation'
-        }
-
-        stages {
-
+        node('workstation') {
+            stage('CleanUp'){
+                cleanWs()
+            }
             stage('compile/build') {
-                steps {
-                   script {
-                       common.compile()
-                   }
-                }
+                common.compile()
             }
-
-            stage('unit tests') {
-                steps {
-                    script {
-                        common.unittests()
-                    }
-                }
+            stage('unittests') {
+                common.unittests()
             }
-
             stage('quality control') {
-                environment {
-
-
-                    SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                    SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                }
-                steps {
-
+                SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
                     sh "sonar-scanner -Dsonar.host.url=http://172.31.12.130:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
                 }
             }
             stage('upload code to centralised place') {
-                steps {
-                    echo 'upload'
-                }
+                echo 'upload'
             }
-
         }
-
+    } catch (Exception e) {
+        common.email("failed")
     }
+}
 
 
-} catch(Exception g) {
-      common.email("failed")
-    }
-    }
+
+
+
+
+
+
+
+
+
+
+
