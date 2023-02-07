79727d04620aa78b568e5f0878dd1130ed13dcca
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-07 11:18:27 +0530

    frontend jenkins

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,16 +1,22 @@
 def call() {
+    if(!env.SONAR_EXTRA_OPTS) {
+        env.SONAR_EXTRA_OPTS = " "
+    }
     try {
         node('workstation') {
-            stage('Checkout'){
+
+            stage('Checkout') {
                 cleanWs()
                 git branch: 'main', url: "https://github.com/pavanikakarlapudi/${component}.git"
             }
+
             stage('compile/build') {
                 common.compile()
             }
             stage('unittests') {
                 common.unittests()
             }
+
             stage('quality control') {
                 SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                 SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
