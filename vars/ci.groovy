c9dd40494b12d1d9c2d24403b79b2f4f19b56aef
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-08 11:20:36 +0530

    frontend jenkins

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,42 +1,53 @@
 def call() {
+
     if(!env.SONAR_EXTRA_OPTS) {
         env.SONAR_EXTRA_OPTS = " "
     }
+    if(!env.TAG_NAME) {
+        env.PUSH_CODE = "false"
+    } else {
+        env.PUSH_CODE = "true"
+    }
+
     try {
         node('workstation') {
 
             stage('Checkout') {
                 cleanWs()
-                git branch: 'main', url: "https://github.com/pavanikakarlapudi/${component}.git"
+                git branch: 'main', url: "https://github.com/pavanikakarlapudi/${component}"
+                sh 'env'
             }
 
-            stage('compile/build') {
+            stage('Compile/Build') {
                 common.compile()
             }
-            stage('unittests') {
+
+            stage('Unit Tests') {
                 common.unittests()
             }
 
-            stage('quality control') {
-                SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+            stage('Quality Control') {
+                SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                 wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-                    sh "sonar-scanner -Dsonar.host.url=http://172.31.12.130:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
-                    sh "echo sonar scan"
+                    //sh "sonar-scanner -Dsonar.host.url=http://172.31.12.130:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
+                    sh "echo Sonar Scan"
                 }
+            }
 
+
+            if(env.PUSH_CODE == "true") {
+                stage('Upload Code to Centralized Place') {
+                    common.artifactPush()
+                }
             }
-            stage('upload code to centralised place') {
-                echo 'upload'
-            }
+
         }
-    }catch(Exception e) {
-        common.email("failed")
+
+    } catch(Exception e) {
+        common.email("Failed")
     }
 }
-
-
-
 
 
 
