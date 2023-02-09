bd531f156cc36f8032e97326845979ed09cf3085
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-10 00:03:55 +0530

    frontend jenkins

--- vars/common.groovy
+++ vars/common.groovy
@@ -27,7 +27,7 @@ def unittests() {
 }
 
 def email(email_note) {
-    mail bcc: '', body: "Job Failed - ${JOB_BASE_NAME}\nJenkins URL - ${JOB_URL}", cc: '', from: 'pavanikakarlapudi1990@gmail.com', replyTo: '', subject: "Jenkins Job Failed - ${JOB_BASE_NAME}", to: 'raghuk.vit@gmail.com'
+    mail bcc: '', body: "Job Failed - ${JOB_BASE_NAME}\nJenkins URL - ${JOB_URL}", cc: '', from: 'pavanikakarlapudi1990@gmail.com', replyTo: '', subject: "Jenkins Job Failed - ${JOB_BASE_NAME}", to: 'pavanikakarlapudi1990@gmail.com'
 }
 
 def artifactPush() {
